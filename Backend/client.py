bb71d54e4c7a072db787fc684b94a6f652e807f1
pranshu-kumar <pranshu0053@gmail.com>
2020-11-20 08:59:51 +0530

    server and client classes made (multi-threaded)

--- Backend/client.py
+++ Backend/client.py
@@ -1,19 +1,34 @@
 '''
-Python code to define Client
-
-CLIENT REQUEST MESSAGE:
-------------------------
-req_msg = {
-    command: "LOGIN/LOGOUT/FETCH...",
-    header_lines: {
-        server_id: server_id,
-        accept_encoding: 'utf-8',
-        ... 
-    },
-    body: 'data'
-}
+Python code to define Multi-threaded Client 
 '''
 
+import socket
+
 class Client():
-    def __init__(self):
-        pass
+    def __init__(self, host, PORT):
+        self.host = host
+        self.PORT = PORT
+
+    '''
+    Create Client Socket
+    '''
+    def create_client_socket(self):
+        self.client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        print("Client Socket Created!")
+        return self.client_socket
+
+    '''
+    Client Socket Connect
+    '''
+    def client_socket_connect(self):
+        self.client_socket.connect((self.host, self.PORT))
+
+    '''
+    Client Send and Receive
+    '''
+    def client_snd_and_rcv(self):
+        while(1):
+            # Perform the necessary functions of send and receive 
+            # on the client side
+
+        self.client_socket.close()
--- Backend/server.py
+++ Backend/server.py
@@ -1,27 +1,71 @@
 '''
-Python code to define Server
-
-SERVER RESPONSE MESSAGE:
-------------------------
-response_msg = {
-    status_line: {
-        protocol: 'TCP/UDP',
-        status_code: 200/301...
-    },
-    header_lines: {
-        date: date,
-        accept_ranges: bytes,
-        content_length: content_length,
-        keep_alive: {
-            timeout: 10,
-            max: 100
-        },
-        connection: 'keep-alive'
-    }
-    data: data
-}
+Python code to define Multi-Threaded Server
 '''
 
+import socket
+from _thread import *
+import threading
+
+print_lock = threading.Lock()
+
 class Server():
-    def __init__(self):
-        pass
+    def __init__(self, PORT, host):
+        self.PORT = PORT
+        self.host = host
+
+    '''
+    Create a server socket
+    '''
+    def create_server_socket(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        return server_socket
+
+
+    '''
+    Bind Socket
+    '''
+    def bind_server_socket(self, host):   
+        self.server_socket.bind((self.host, self.PORT))
+        print("Socket Binded to port {}!".format(self.host))
+
+    '''
+    Socket Listening
+    '''
+    def server_socket_listen(self):
+        self.server_socket.listen(5)
+        print("Server Socket is listening")
+
+
+    '''
+    Server Socket Accept
+    '''
+    def server_socket_accept(self):
+        # Establish connection with client
+        client, addr = self.server_socket.accept()
+
+        # Client acquires lock
+        print_lock.acquire()
+        print("Connected to client: " addr[0])
+        return client, addr
+
+    '''
+    Connect with Clients and perform Send and Receive
+    '''
+    def connect_to_clients(self):
+        while(1):
+            
+            client, addr = self.server_accept()
+            
+            # Perform Threaded Send and Receive
+            start_new_thread(self.server_snd_and_rcv(), (client,))
+
+        self.server_socket.close()
+
+    '''
+    Server side send and receive
+    '''
+    def server_snd_and_rcv(self, client):
+        while(1):
+            # Send and Receive
+            # Necessary functions for sending and accepting req/response to be added here
+        client.close()
